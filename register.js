85285f6e3d952a262785aa83dd6a92e9bd1ef5b9
braveSugar <braveSugar@outlook.com>
2024-11-22 21:10:06 +0800

    beatified the about、founder、privacy、login、register page

--- register.js
+++ register.js
@@ -1,5 +1,17 @@
 const submitBtnList = document.querySelectorAll(".submit-btn");
 
+// const hideAll
+
+const showElem = (elem) => {
+  console.log(elem);
+  // elem.hidden = false;
+  elem.classList.remove("hidden");
+};
+const hideElem = (elem) => {
+  // elem.hidden = true;
+  elem.classList.add("hidden");
+};
+showElem(document.querySelector('[class^="step-1"]'));
 submitBtnList.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const stepElems = [...document.querySelectorAll('[class^="step-"]')];
@@ -13,9 +25,8 @@ submitBtnList.forEach((btn) => {
     if (currStep.nextElementSibling) {
       console.log(currStep.nextElementSibling);
 
-      currStep.hidden = true;
-
-      currStep.nextElementSibling.hidden = null;
+      hideElem(currStep);
+      showElem(currStep.nextElementSibling);
     }
   });
 });
